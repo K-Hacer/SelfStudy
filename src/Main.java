ba674fe4170da799b931ee63d5a9b821864cec18
K-Hacer <yamitjohnmark232gmail.com>
2025-04-26 21:41:16 +0800

    init commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main{
+    public static void main(String[] args) {
+
+        int age = 19;
+        double x = 20.9;
+        double price = 10.2;
+        double temp = -16.5;
+
+        char grade = 'A';
+        char symbol = '!';
+        char currency = '$';
+
+        boolean isStudent = false;
+        boolean forSale = false;
+        boolean isOnline = true;
+
+        if (isStudent){
+        System.out.println("You are a Student!");
+    }
+    else {
+        System.out.println("You are NOT a student!");
+        }
+    }
+}
