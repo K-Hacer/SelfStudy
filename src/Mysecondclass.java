635cdfc601925cfd328d8521e8b10c71743f5ba3
K-Hacer <yamitjohnmark232gmail.com>
2025-04-29 17:26:31 +0800

    init commit

--- src/Mysecondclass.java
+++ src/Mysecondclass.java
@@ -1,14 +1,24 @@
 import java.util.Scanner;
 
 public class Mysecondclass {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+    // calculate of te area of rectangle
+        double width = 0;
+        double height = 0;
+        double area = 0;
+
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
-        System.out.print("Enter your age: ");
-        int age = scanner.nextInt();
-        System.out.print("Hello " + name);
-        System.out.print("\tYour age is " + age);
-        scanner.close();
+
+        System.out.print("Enter the Width: ");
+        width = scanner.nextDouble();
+
+        System.out.print("Enter the Height: ");
+        height = scanner.nextDouble();
+
+        System.out.println("The Width is " + width);
+
+        area = width * height;
+
+        System.out.println("The total of Area is " + area + "cm");
     }
 }
