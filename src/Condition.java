c8d77c9d5d773535c34a50d0276e4fbc05578984
K-Hacer <yamitjohnmark232gmail.com>
2025-05-15 13:16:59 +0800

    init commit

---
+++ src/Condition.java
@@ -0,0 +1,21 @@
+
+public class Condition {
+    public static void main(String[] args){
+
+        //Ternary operator ? = Return 1 of 2 variable if a condition is true
+
+        // variable = (condition) ? ifTrue : ifFalse;
+
+        /* int hours = 6;
+
+        String timeOfDay = (hours > 12) ? "A.M. " : "P.M. ";
+        System.out.println(timeOfDay);
+
+         */
+
+        int income = 30000;
+
+        Double TaxRate = (income >= 20000) ? 0.23 : 2.56;
+        System.out.println(TaxRate);
+    }
+}
